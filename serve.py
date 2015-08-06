3035da53b0f1eef237e3a6f7c9823ae8237d3bd7
Jamie Lentin <jm@lentin.co.uk>
2015-08-06 11:42:33 +0100

    Use constants to define ports

--- serve.py
+++ serve.py
@@ -4,13 +4,21 @@ from socketio.server import SocketIOServer
 from pyramid.paster import get_app
 from gevent import monkey; monkey.patch_all()
 
+INTERFACE = '0.0.0.0'
+PORT_HTTP = 8090
+PORT_POLICY = 10843
+
 def main():
     app = get_app(os.path.join(os.path.dirname(__file__), 'development.ini'))
-    print 'Listening on port http://0.0.0.0:8080 and on port 10843 (flash policy server)'
+    print 'Listening on port http://%s:%d and on port %d (flash policy server)' % (
+        INTERFACE,
+        PORT_HTTP,
+        PORT_POLICY,
+    )
 
-    SocketIOServer(('0.0.0.0', 8090), app,
+    SocketIOServer((INTERFACE, PORT_HTTP), app,
         resource="socket.io", policy_server=True,
-        policy_listener=('0.0.0.0', 10843)).serve_forever()
+        policy_listener=(INTERFACE, PORT_POLICY)).serve_forever()
 
 if __name__ == '__main__':
     main()
